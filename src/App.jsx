7176fd5672a066ee31b75bab930c1c86d86bccfe
Edwin Martinez <ing.ed.mtz.leon@gmail.com>
2024-01-30 23:53:26 -0600

    Se quita codigo inecesario

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import { Footer } from "./Footer"
-import { Navbar } from "./Navbar"
-import './App.css'
-
-function App() {
-  
+const App= ()=> {  
   return (
     <>
       <div>
-        <Navbar />
-        <h1 style={{ color:"saddlebrown", fontSize:"2rem" }}>Hola</h1>
+        <h1>Esta todo limpio</h1>
       </div>
-      <Footer/>
     </>
   )
 }
