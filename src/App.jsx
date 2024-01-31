dfe25934fcd938afa4b6b8ff75c14eff6b30324a
Edwin Martinez <ing.ed.mtz.leon@gmail.com>
2024-01-30 23:33:50 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Footer } from "./Footer"
+import { Navbar } from "./Navbar"
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+        <Navbar />
+        <h1 style={{ color:"saddlebrown", fontSize:"2rem" }}>Hola</h1>
+      </div>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+
+
+export const Navbar = () =>{
+    return(
+    <>
+        <nav>
+            <ul>
+                <li>Productos</li>
+                <li>Clientes</li>
+                <li>Conocer mas</li>
+            </ul>
+        </nav>
+    </>
+    )
+}
