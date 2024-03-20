d6142df2e567aa37ee1e4a361beb562172443c24
edwinML <ing.ed.mtz.leon@gmail.com>
2024-03-19 19:22:57 -0600

    clase 9

--- src/components/pages/formFormik/FormFormik.jsx
+++ src/components/pages/formFormik/FormFormik.jsx
@@ -1,38 +1,83 @@
 import { Box, Button, TextField } from "@mui/material"
+import { useFormik } from 'formik';
+import  * as Yup from 'yup';
+
 
 export const FormFormik = () => {
+
+  const { handleSubmit , handleChange, errors } = useFormik({
+    initialValues: {
+        name: "",
+        email:"",
+        password:"",
+        changePassword:""
+    },
+    onSubmit: ( data ) => {
+        console.log(data);
+    },
+    validationSchema:Yup.object({
+        email: Yup.string()
+        .email("Debes ingresar un email valido")
+        .required("Campo requerido"),
+
+        password: Yup.string()
+        .required("Campo requerido")
+        .matches(/^(?=.*[0-9])(?=.*[A-Z]).{7,}$/ , "Debe tener una mayuscula, un número y minimo de 7 caracteres" ),
+
+        changePassword: Yup.string()
+        .required("Campo requerido")
+        .oneOf( [Yup.ref("password")] , "Las constraseñas no coinciden" ),
+    }) ,
+    validateOnChange: false,
+  })
+
+  console.log(errors);
+
   return (
     <div style={{ padding:"40px", display:"flex", justifyContent:"center" }}>
-        <form action=""
+        <form 
+            onSubmit={ handleSubmit }
             style={{
                 display: "flex",
                 flexDirection:"column",
                 width:"50%",
                 gap: "20px"
-            }}>
+            }}
+            >
         <TextField
-          id="standard-multiline-flexible"
           label="Nombre"
           multiline
+          name="name"
           variant="standard"
+          onChange={handleChange}
         />
         <TextField
-          id="standard-textarea"
+          error={errors.email ? true : false }  
           label="Email"
           multiline
+          name="email"
           variant="standard"
+          onChange={handleChange}
+          helperText={ errors.email  }
         />
         <TextField
-          id="standard-multiline-static"
+          error={errors.password ? true : false }  
           label="Contraseña"
           multiline
+          name="password"
           variant="standard"
+          onChange={handleChange}
+          helperText={ errors.password  }
+
         />
          <TextField
-          id="standard-multiline-static"
+          error={errors.changePassword ? true : false }  
           label="Confirmar contraseña"
           multiline
+          name="changePassword"
           variant="standard"
+          onChange={handleChange}
+          helperText={ errors.changePassword  }
         />
         <Box sx={{ width: "100%", display:"flex", justifyContent:"flex-end" }}>
             <Button type="submit" variant="contained" sx={{width:200}}>
